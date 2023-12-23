7a6f6be5bdb34214e3c129c196c18265bded73ce
chuyishang <chuyishang@berkeley.edu>
2023-12-23 03:53:07 +0000

    new dataset file

--- dataset.py
+++ dataset.py
@@ -1,5 +1,77 @@
-import csv
 
+import csv
+from torch.utils.data import Dataset
+import pandas as pd
+import os
+
+from urllib.request import urlopen
+from PIL import Image
+import decord
+from decord import cpu, gpu
+import numpy as np
+import torchvision.transforms as T
+import ast
+class MyDataset(Dataset):
+    def __init__(self, data_path="", query_file="", image_transforms=None, fps=30, start_sample=0, max_samples=None):
+        """Args:- path: path to the dataset
+            - image_transforms: image transforms to apply to the frames
+            - fps: fps of the video
+            - query_file: path to the query file
+            - max_samples: maximum number of samples to use"""
+        self.path = path
+        self.image_transforms = image_transforms
+        self.fps = fps
+        self.query_file = query_file
+        self.start_sample = 0
+        self.max_samples = max_samples
+
+        with open(self.query_file) as f:
+            self.df = pd.read_csv(f, index_col=None, keep_default_na=False)
+
+        if self.max_samples is not None:
+            self.df = self.df[start_sample : start_sample + self.max_samples]
+        
+        self.length = len(self.df)
+    
+    def get_sample_path(self, index):
+        return os.path.join(self.path, self.df.iloc[index]["video_name"])
+
+    def get_video(self, video_path, fps=30, sample_freq=None):
+        """Gets a video and returns it as a tensor.
+            - video_path: path to the video
+            - fps: fps of the video
+            - sample_freq: frequency of sampling the video frames. sample_freq=10 means every 10th frame is sampled. if None, all frames are sampled
+        """
+         # If fixed width and height are required, VideoReader takes width and height as arguments.
+        video_reader = decord.VideoReader(str(video_path), num_threads=1, ctx=cpu(0))
+        decord.bridge.set_bridge('torch')
+        vlen = len(video_reader)
+        #print(vlen)
+        original_fps = video_reader.get_avg_fps()
+        #print(original_fps)
+        num_frames = int(vlen * fps / original_fps)
+        # num_frames = min(self.max_num_frames, num_frames)
+        frame_idxs = np.linspace(0, vlen, num_frames, endpoint=False).astype(np.int_)
+        if sample_freq:
+            frame_idxs = frame_idxs[::sample_freq]
+        video = video_reader.get_batch(frame_idxs).byte()
+        video = video.permute(0, 3, 1, 2)
+        return video
+    
+    def __get__item__(self, index):
+        out_dict = self.df.iloc[index].to_dict()
+        sample_path = self.get_sample_path(index)
+        # Load and transform image
+        video = self.get_video(sample_path)
+        out_dict["video"] = video
+        out_dict["index"] = index
+        return out_dict
+
+    def __len__(self):
+        return self.length
+
+
+"""
 def get_data(filename):
     ids = []
     choices =  []
@@ -18,4 +90,4 @@ def get_data(filename):
             answers.append(answer)
             vid_names.append(video_name)
     return ids, choices, queries, answers, vid_names
-    
+"""
